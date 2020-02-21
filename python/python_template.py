9c88041772951c82a82a048fca678ff111e96910
Sid-Kr1 <56985355+Sid-Kr1@users.noreply.github.com>
2020-02-21 14:10:38 +1100

    updated import statements and formatted with black

--- python/python_template.py
+++ python/python_template.py
@@ -1,25 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 __script_name__ = "NAME ME"
 __version__ = "0.0.1"
-__maintainer__ = "Michael Imelfort"
-__email__ = "mike@mikeimelfort.com"
+__maintainer__ = "Sid Krishnan"
+__email__ = ""
 __status__ = "Development"
 
-import argparse
-import sys
+# import argparse
+# import concurrent.futures  # for concurrent.futures.ProcessPoolExecutor()
+# import errno
+# import sys
+# from pathlib import Path  # Python3 libs for os.path
+# from shutil import rmtree
+# from subprocess import PIPE, Popen
 
-#from multiprocessing import Pool
-#from subprocess import Popen, PIPE
+# import dask.dataframe as dd
+# import numpy as np
+# import pandas as pd
+# import plotly.express as px
+# import plotly.graph_objects as go
 
-#import os
-#import errno
+# np.seterr(all='raise')
 
-#import numpy as np
-#np.seterr(all='raise')
-
-#import matplotlib as mpl
-#import matplotlib.pyplot as plt
 
 def run_command(cmd):
     """Run a command and take care of stdout
@@ -28,10 +30,11 @@ def run_command(cmd):
 
     returns (stdout, stderr)
     """
-    p = Popen(cmd.split(' '), stdout=PIPE)
+    p = Popen(cmd.split(" "), stdout=PIPE)
     return p.communicate()
 
-def do_work( args ):
+
+def do_work(args):
     """ Main wrapper"""
 
     """
@@ -89,49 +92,55 @@ def do_work( args ):
 
     return 0
 
+
 ###############################################################################
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help="Input file to parse")
-    #parser.add_argument('positional_arg', help="Required")
-    #parser.add_argument('positional_arg2', type=int, help="Integer argument")
-    #parser.add_argument('positional_arg3', nargs='+', help="Multiple values")
-    #parser.add_argument('-X', '--optional_X', action="store_true", default=False, help="flag")
+    parser.add_argument("filename", help="Input file to parse")
+    # parser.add_argument('positional_arg', help="Required")
+    # parser.add_argument('positional_arg2', type=int, help="Integer argument")
+    # parser.add_argument('positional_arg3', nargs='+', help="Multiple values")
+    # parser.add_argument('-X', '--optional_X', action="store_true", default=False, help="flag")
 
-    #-------------------------------------------------
+    # -------------------------------------------------
     # get and check options
     args = None
-    if(len(sys.argv) == 1):
+    if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(0)
-    elif(sys.argv[1] == '-v' or \
-         sys.argv[1] == '--v' or \
-         sys.argv[1] == '-version' or \
-         sys.argv[1] == '--version'):
+    elif (
+        sys.argv[1] == "-v"
+        or sys.argv[1] == "--v"
+        or sys.argv[1] == "-version"
+        or sys.argv[1] == "--version"
+    ):
         print "%s: version: %s" % (__script_name__, __version__)
         sys.exit(0)
-    elif(sys.argv[1] == '-h' or \
-         sys.argv[1] == '--h' or \
-         sys.argv[1] == '-help' or \
-         sys.argv[1] == '--help'):
+    elif (
+        sys.argv[1] == "-h"
+        or sys.argv[1] == "--h"
+        or sys.argv[1] == "-help"
+        or sys.argv[1] == "--help"
+    ):
         parser.print_help()
         sys.exit(0)
     else:
         args = parser.parse_args()
-    
+
     try:
-        if(__profiling__):
+        if __profiling__:
             import cProfile
-            cProfile.run('do_work(args)', 'prof')
+
+            cProfile.run("do_work(args)", "prof")
             ##########################################
             ##########################################
             # Use this in python console!
-            #import pstats
-            #p = pstats.Stats('prof')
-            #p.sort_stats('cumulative').print_stats(10)
-            #p.sort_stats('time').print_stats(10)
+            # import pstats
+            # p = pstats.Stats('prof')
+            # p.sort_stats('cumulative').print_stats(10)
+            # p.sort_stats('time').print_stats(10)
             ##########################################
             ##########################################
         else:
@@ -139,4 +148,3 @@ if __name__ == '__main__':
     except:
         print "Unexpected error:", sys.exc_info()[0]
         raise
-
